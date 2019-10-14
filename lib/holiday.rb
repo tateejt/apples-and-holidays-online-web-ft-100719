5354bd5d28116d3ae31916faa79cd343136a726e
Emery Tate <tateejt@gmail.com>
2019-10-14 01:16:27 +0000

    Done.

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -54,7 +54,7 @@ def all_supplies_in_holidays(holiday_hash)
   #   Fourth Of July: Fireworks, BBQ
   # etc.
 
-end
+
 
 def all_holidays_with_bbq(holiday_hash)
   # return an array of holiday names (as symbols) where supply lists
